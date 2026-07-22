38e9cb77cfd3b2dbc2ba8810f67bba199cbe9713
Pizzamaster2026 <datteln_2006@tutamail.com>
2026-07-22 17:38:48 +0200

    script.js erstellt

--- script.js
+++ script.js
@@ -1,1 +1,24 @@
-console.log("PizzaMasterLite gestartet");
+const pizzas = document.getElementById("pizzas");
+const weight = document.getElementById("weight");
+const hydration = document.getElementById("hydration");
+const salt = document.getElementById("salt");
+
+const flour = document.getElementById("flour");
+const water = document.getElementById("water");
+const saltResult = document.getElementById("saltResult");
+const total = document.getElementById("total");
+
+function calculate() {
+
+    const dough = Number(pizzas.value) * Number(weight.value);
+
+    total.textContent = dough.toFixed(0) + " g";
+
+}
+
+calculate();
+
+pizzas.addEventListener("input", calculate);
+weight.addEventListener("input", calculate);
+hydration.addEventListener("input", calculate);
+salt.addEventListener("input", calculate);
